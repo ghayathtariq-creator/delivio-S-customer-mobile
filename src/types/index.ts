595f869601a3f870332e927eeb3e3f170bad86e4
ghayathtariq-creator <ghayath.tariq@gmail.com>
2026-03-06 10:45:44 +0200

    Create TypeScript type definitions for the app

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+// TypeScript type definitions for the delivio-S-customer-mobile app
+
+type User = {
+    id: string;
+    name: string;
+    email: string;
+};
+
+type Product = {
+    id: string;
+    name: string;
+    price: number;
+};
+
+export type { User, Product };
